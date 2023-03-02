1dcdd3b1440c8e4b1ccab415ce8c3cc8e5255c41
MaksKuchuk <maks.kuchuk.2016@gmail.com>
2023-03-03 00:26:00 +1000

    read filename

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,5 +13,12 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+void MainWindow::on_actionNew_file_triggered()
+{
+    QString str;
+    str = QFileDialog::getOpenFileName();
+
+    ui->statusbar->showMessage(str);
 }
 
