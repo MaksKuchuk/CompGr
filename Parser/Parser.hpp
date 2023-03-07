91552d7b4c23264532b9faeb9b3610cd84e0e0a1
MrGriboman <96828637+MrGriboman@users.noreply.github.com>
2023-03-08 00:25:15 +1000

    parser change

--- Parser/Parser.hpp
+++ Parser/Parser.hpp
@@ -1,20 +1,26 @@
 #include "ParseDataTxt.hpp"
+#include "ParseDataWav.hpp"
 
 #ifndef PARSER_H
 #define PARSER_H
 
 class Parser {
 public:
-    static ParseData *parse(const std::string &path_to_file) {
+    static ParseData* parse(const std::string& path_to_file) {
         std::ifstream file_to_parse(path_to_file);
- 
+
         unsigned long long dotPos = path_to_file.rfind('.');
         std::string extension = path_to_file.substr(dotPos + 1, path_to_file.size() - 1);
- 
+
         if (extension == "txt") {
             auto pd = new ParseDataTxt();
             pd->parse(file_to_parse);
-            return static_cast<ParseData *>(pd);
+            return static_cast<ParseData*>(pd);
+        }
+        else if (extension == "wav") {
+            auto pd = new ParseDataWav();
+            pd->parse(file_to_parse);
+            return static_cast<ParseData*>(pd);
         }
         return nullptr;
     }
