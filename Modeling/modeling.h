88bd53fa0d92bffb36b127341fda46c1e75e9edb
Dezzelshipc <foler126@gmail.com>
2023-03-31 17:23:05 +1000

    modeling header

--- Modeling/modeling.h
+++ Modeling/modeling.h
@@ -4,26 +4,47 @@
 #include<cmath>
 
 #include "../GraphGlData/Graph2DData.hpp"
+
 class Modeling {
 public:
-    static Graph2DData* delayedSingleImpulse(const long long N, const double T, const long long n);
+    static Graph2DData *delayedSingleImpulse(const long long N, const double T, const long long n);
 
-    static Graph2DData* delayedSingleHop(const long long N, const double T, const long long n);
+    static Graph2DData *delayedSingleHop(const long long N, const double T, const long long n);
 
-    static Graph2DData* sampledDecreasingExponent
+    static Graph2DData *sampledDecreasingExponent
             (
-                const long long N, const double T, const int a
+                    const long long N, const double T, const int a
             );
 
-    static Graph2DData* sampledSineWave
-        (
-            const long long N, const double T,
-            const int a, const double omega, const double phi
-        );
+    static Graph2DData *sampledSineWave
+            (
+                    const long long N, const double T,
+                    const int a, const double omega, const double phi
+            );
 
-    static Graph2DData* meander(const long long N, const double T, const long long L);
+    static Graph2DData *meander(const long long N, const double T, const long long L);
 
-    static Graph2DData* saw(const long long N, const double T, const long long L);
+    static Graph2DData *saw(const long long N, const double T, const long long L);
+
+    static Graph2DData *exponentialEnvelope(
+        const long long N, const double T, const double a,
+        const double tau, const double f, const double phi
+    );
+
+    static Graph2DData *balanceEnvelope(
+            const long long N, const double T, const double a,
+            const double f0, const double fn, const double phi
+    );
+
+    static Graph2DData *tonalEnvelope(
+            const long long N, const double T, const double a,
+            const double f0, const double fn, const double phi, const double m
+    );
+
+    static Graph2DData *LFM(
+            const long long N, const double T, const double a,
+            const double f0, const double fk, const double phi0
+    );
 };
 
 #endif // MODELING_H
