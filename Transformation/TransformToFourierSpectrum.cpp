998c6d185763f1ff72bac584add79aae3f70c749
MrGriboman <96828637+MrGriboman@users.noreply.github.com>
2023-05-19 22:24:30 +1000

    fourier

--- Transformation/TransformToFourierSpectrum.cpp
+++ Transformation/TransformToFourierSpectrum.cpp
@@ -1,10 +1,6 @@
 #include "TransformToFourierSpectrum.hpp"
 
-#include <cmath>
-#include <limits>
-#include <iostream>
-
-void TransformToFourierSpectrum::FFTAnalysis(QList<double>& AVal, QList<double>& FTvl, int Nvl, int Nft) {
+/*void TransformToFourierSpectrum::FFTAnalysis(QList<double>& AVal, QList<double>& FTvl, int Nvl, int Nft) {
      const double TwoPi = 6.283185307179586;
      int i, j, n, m, Mmax, Istp;
      double Tmpr, Tmpi, Wtmp, Theta;
@@ -61,45 +57,121 @@ void TransformToFourierSpectrum::FFTAnalysis(QList<double>& AVal, QList<double>&
      }
 
      delete []Tmvl;
-   }
+}*/
 
-std::shared_ptr<Graph2DData> TransformToFourierSpectrum::transform(std::shared_ptr<GeneralData> data, long long n) {
-    long long size = data->getAmountOfSamples();
-    long long new_size = std::pow(2, std::ceil(std::log2(size)));
-
-    QList<double> AVal(data->getChannel(n));
-    AVal.resize(new_size);
-    QList<double> FTvl(new_size);
-    FFTAnalysis(AVal, FTvl, new_size, new_size);
-
-    double min = FTvl[0];
-    double max = FTvl[0];
-    for (long long i = 0; i < new_size; ++i) {
-        if (FTvl[i] < min) {
-            min = FTvl[i];
+void TransformToFourierSpectrum::smoothing(QList<double> data, long long L, size_t size) {
+    for (size_t i = 0; i < size; ++i) {
+        long long sum = 0;
+        for (size_t j = -L; j <= L; ++j) {
+            sum += data[i + j];
         }
-        if (FTvl[i] > max) {
-            max = FTvl[i];
+        data[i] = sum / (2*L + 1);
+    }
+}
+
+void TransformToFourierSpectrum::amplitudeSpectrum(CArray& FTvl, QList<double> new_data, long long size, double T) {
+    for (size_t i = 0; i < size; ++i) {
+        new_data[i] = T * abs(FTvl[i]);
+    }
+}
+
+void TransformToFourierSpectrum::PSDSpectrum(CArray& FTvl, QList<double> new_data, long long size, double T) {
+    for (size_t i = 0; i < size; ++i) {
+        new_data[i] = std::pow(T, 2) * std::pow(abs(FTvl[i]), 2);
+    }
+}
+
+void TransformToFourierSpectrum::first_val_change(CArray& x, int mode) {
+    if (mode == EQUALIZE_WITH_ADJ)
+        x[0] = abs(x[1]);
+    else if (mode == ZERO_FIRST_VAL)
+        x[0] = 0;
+}
+
+void TransformToFourierSpectrum::FFTW(CArray& x) {
+    const size_t N = x.size();
+    if (N <= 1)
+        return;
+
+    // divide
+    CArray even = x[std::slice(0, N/2, 2)];
+    CArray odd = x[std::slice(1, N/2, 2)];
+
+    // conquer
+    FFTW(even);
+    FFTW(odd);
+
+    // combine
+    for (size_t k = 0; k < N/2; ++k) {
+        Complex t = std::polar(1.0, -2 * PI * k / N) * odd[k];
+        x[k] = even[k] + t;
+        x[k+N/2] = even[k] - t;
+    }
+}
+
+std::shared_ptr<Graph2DData> TransformToFourierSpectrum::transform
+    (
+        std::shared_ptr<GeneralData> data,
+        long long n,
+        long long L,
+        int mode,
+        int first_val_mode,
+        long long lcur,
+        long long rcur
+    )
+{
+    //long long size = data->getAmountOfSamples();
+    //long long new_size = std::pow(2, std::ceil(std::log2(size)));
+    long long new_size = rcur - lcur;
+
+    QList<double> vals(data->getChannel(n));
+    vals.resize(new_size);
+    Complex* buf = new Complex[new_size];
+    for (size_t i = 0; i < new_size; ++i) {
+        buf[i] = vals[i];
+    }
+    CArray FTvl(buf, new_size);
+    delete[] buf;
+
+    FFTW(FTvl);
+    if (first_val_mode != KEEP_FIRST_VAL)
+        first_val_change(FTvl, first_val_mode);
+
+    QList<double> new_data(new_size);
+
+    if (mode == AMPLITUDE_SPECTRUM)
+        amplitudeSpectrum(FTvl, new_data, new_size, 1 / data->getHz());
+    else if (mode == PSD)
+        PSDSpectrum(FTvl, new_data, new_size, 1 / data->getHz());
+
+    if (L)
+        smoothing(new_data, L, new_size);
+
+    double min = 0;
+    double max = new_data[0];
+    for (long long i = 0; i < new_size; ++i) {        
+        if (new_data[i] > max) {
+            max = new_data[i];
         }
     }
 
     new_size = new_size / 2;
     if (new_size == 0) new_size = 1;
-    FTvl.resize(new_size);
+    new_data.resize(new_size);
 
     auto data2d = std::make_shared<Graph2DData>();
 
     data2d->Hz = 1 / (new_size);
     data2d->totalSeconds = 1;
     data2d->amountOfSamples = new_size;
-    data2d->samples = std::move(FTvl);
+    data2d->samples = (new_data);
     data2d->lcur = 0;
     data2d->rcur = (new_size) - 1;
     data2d->name = data->getChannelName(n) + QString::fromStdString( " Fourier transform" );
     data2d->minVal = min;
-    data2d->maxVal = max / 10;
+    data2d->maxVal = max;
     data2d->minLoc = min;
-    data2d->maxLoc = max / 10;
+    data2d->maxLoc = max;
 
     return data2d;
 }
--- Transformation/TransformToFourierSpectrum.hpp
+++ Transformation/TransformToFourierSpectrum.hpp
@@ -4,12 +4,39 @@
 #include "../Parser/ParseData.hpp"
 #include "../GraphGlData/Graph2DData.hpp"
 
+#include <complex>
+#include <valarray>
+#include <cmath>
+#include <limits>
+
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+const double PI = 3.141592653589793238460;
+
+// First val modes
+const int KEEP_FIRST_VAL = 0;
+const int ZERO_FIRST_VAL = 1;
+const int EQUALIZE_WITH_ADJ = 2;
+
+// Spectrum analysis modes
+const int AMPLITUDE_SPECTRUM = 0;
+const int PSD = 1;
+
 class TransformToFourierSpectrum {
     static void FFTAnalysis(QList<double>& AVal, QList<double>& FTvl, int Nvl, int Nft);
+
+    static void FFTW(CArray& x);
+
+    static void first_val_change(CArray& x, int mode);
+
+    static void amplitudeSpectrum(CArray&, QList<double>, long long, double);
+
+    static void PSDSpectrum(CArray&, QList<double>, long long, double);
+
+    static void smoothing(QList<double>, const long long, size_t);
+
 public:
-
-    static std::shared_ptr<Graph2DData> transform(std::shared_ptr<GeneralData> data, long long n);
-
+    static std::shared_ptr<Graph2DData> transform(std::shared_ptr<GeneralData> data, long long n, long long L, int mode, int, long long, long long);
 };
 
 #endif
